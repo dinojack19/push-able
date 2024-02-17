c6d08171a5a6378a48e05185b3304c06355511c4
JACK CUTLER <jack.cutler.b@gmail.com>
2024-02-18 10:39:33 +1300

    adding data

--- leson_5.py
+++ leson_5.py
@@ -2,12 +2,9 @@ import math
 import pandas as pd
 
 
-#data = pd.read_csv('Kumpula-June-2016-w-metadata.txt', sep=',', header=None)
-data = pd.read_csv("data\Kumpula-June-2016-w-metadata (1).cvs",  sep=',',  skiprows=8)
-
-temp_data = data[["TEMP","MIN","MAX"]].plot()
+#data = pd.read_csv('Kumpula-June-2016-w-metadata.txt', sep=',')
+data = pd.read_csv("data\6153237444115dat.csv")
 
 
-
-print(temp_data)
+print(data)
 
